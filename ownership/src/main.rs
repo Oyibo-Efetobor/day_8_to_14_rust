118d984047800185f2cadbf8499a77a35634ea22
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-07 00:15:33 +0100

    main

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,16 @@
+//Ownership
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("RUST");
+    //the 'RUST' value is owned by the s1 owner
+
+    let len = calculate_length(&s1); //not passing the owner but a ref to the owner
+    println!("Length of '{}' is {}.", s1, len);
+}
+
+
+// s in calculate_length is not going to
+//be an owner but a reference to s1
+// and reference is proceeded by an ampersand sign & 
+fn calculate_length(s: &String) -> usize{
+    s.len()
 }
