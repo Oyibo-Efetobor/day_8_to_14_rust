19aaa9f1f30bee1fa60fbb777ca103951526814b
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-11 23:43:31 +0100

    main

--- v_m/src/main.rs
+++ v_m/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
 
     // _a = 10; //this will throw an error because _a is immutable
 
+    let mut _b: i32 = 5;
+    println!("b: {}", _b);
+    _b = 10; //this will work because _b is mutable
+    println!("b: {}", _b);
 }
