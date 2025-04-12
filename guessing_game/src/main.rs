066b5d207c8325612bd692a6a8a2610acfe27dfc
OYIBO Efetobor George <efetobor3x@gmail.com>
2025-04-12 13:33:39 +0100

    completed the guessing game code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,18 +4,31 @@ use std::io;
 // rand = '0.9.0' // add this line to your Cargo.toml file
 use rand::Rng; // random number generator
 
+use std::cmp::Ordering; // for comparing numbers
+
 fn main() {
     println!("Welcome to the guessing game!");
+    loop{
+        println!("Please input a guess between 1 and 10");
+        // generate a random number between 1 and 10
+        let secret_number = rand::rng().random_range(1..=10); 
+        
+        let mut guess = String::new(); // mutable variable
+        io::stdin() // standard input
+            .read_line(&mut guess) // read line from standard input
+            .expect("Failed to read line"); // expect will panic if read_line fails
+        println!("You guessed: {guess}");
 
-    let secret_number = rand::rng().random_range(1..=100); // generate a random number between 1 and 100
-
-    println!("The secret number is: {secret_number}");
-
-    println!("Please enter your guess:");
-
-    let mut guess = String::new(); // mutable variable
-    io::stdin() // standard input
-        .read_line(&mut guess) // read line from standard input
-        .expect("Failed to read line"); // expect will panic if read_line fails
-    println!("You guessed: {guess}");
+        // parse the string to u32
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); 
+        // comparing the guess with the secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
